b5469a2ddaab1112401b4067912b8e2054876846
probitie <smstudio1000@gmail.com>
2022-09-16 08:19:14 +0300

    hellotriangle: added vertices processing and vertex shader

--- main.cpp
+++ main.cpp
@@ -50,9 +50,84 @@ int main(void)
         // user input
         processInput(window);
 
-        // rendering stuff
-        //glClearColor(0.2f, 0.6f, 0.5f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+
+        // the rendering stuff happens here
+
+
+        // glClearColor(0.2f, 0.6f, 0.5f, 1.0f);
+        // glClear(GL_COLOR_BUFFER_BIT);
+
+        // === prepare data before rendering === //
+
+        // 2D triangle
+        float vertices[]
+        {
+          -0.5f, -0.5f, 0.0f,
+           0.5f, -0.5f, 0.0f,
+           0.0f,  0.5f, 0.0f
+
+        };
+
+        // vertex buffer objects
+
+        // generate id for the buffer
+        GLuint VBO;
+        glGenBuffers(1, &VBO);
+
+        // now whenever we call GL_ARRAY_BUFFER
+        // the VBO buffer will be invoked
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        // write our triange to that buffer
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+
+        // ==================================== //
+
+
+        // =========== vertex shader ========== //
+
+        // source code
+        const char * vertexShaderSource = "# version 330 core\n"
+                "layout (location = 0) in vec3 aPos;\n"
+                "void main()\n"
+                "{\n"
+                "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                "}\0";
+
+        // shader object id
+        GLuint vertexShader;
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+        // write source code to shader object
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+        // compile it at runtime
+        glCompileShader(vertexShader);
+
+        // check shader compilation errors
+
+        GLint success;
+        const GLint infoLogSize = 512;
+        GLchar infoLog[infoLogSize]{};
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+        if (!success)
+        {
+            glGetShaderInfoLog(vertexShader, infoLogSize, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        //
+
+
+        // ==================================== //
+
+
+        // ========== fragment shader ========= //
+
+        // ==================================== //
 
         // swap buffers, check events
         glfwPollEvents();
