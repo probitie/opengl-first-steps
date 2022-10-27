5607ae7d1a629c9167333eb05927732dc651d809
probitie <viacheslav.mashkov@gmail.com>
2022-10-27 13:44:21 +0300

    added reading pard to shader constructor

--- shader.cpp
+++ shader.cpp
@@ -4,7 +4,6 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 {
     // read vertex/fragment shaders from the path
     // ------------------------------------------
-
     std::string vertexCode, fragmentCode;
     std::ifstream vertexShaderFile, fragmentShaderFile;
 
@@ -12,10 +11,37 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
     vertexShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
     fragmentShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
-    debuglog("first log " << 10);
-    warnlog("second log " << 10);
-    errorlog("3 log " << 10);
+    try
+    {
+        // open files
+        vertexShaderFile.open(vertexPath);
+        fragmentShaderFile.open(fragmentPath);
+
+        // read files into streams
+        std::stringstream vertexStream, fragmentStream;
+        vertexStream << vertexShaderFile.rdbuf();
+        fragmentStream << fragmentShaderFile.rdbuf();
+
+        // close files
+        vertexShaderFile.close();
+        fragmentShaderFile.close();
+
+        // convert streams to strings
+        vertexCode = vertexStream.str();
+        fragmentCode = vertexStream.str();
+    }
+    catch(std::ifstream::failure e)
+    {
+        errorlog("can not read one of shaders: " << e.what());
+    }
+    // -----------------------------------------------------
 
 
+    // compile shaders
+    // -----------------------------------------------------
+    GLuint vertex, fragment;
+    GLint success;
+    char infoLog[512];
 
+    // vertex shader
 }
